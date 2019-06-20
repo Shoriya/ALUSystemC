356ef69640c7b3e292240d86e1aaffc85d578532
Shoriya Chauhan <49990606+Shoriya@users.noreply.github.com>
2019-06-21 00:23:57 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -4,48 +4,45 @@
  int sc_main(int argc, char* argv[])
  {
  sc_signal<int> sig;
+ Testbench_ALU* TBm = new Testbench_ALU("TBm");
  ALU_module* ALUm = new ALU_module("ALUm");
- TestbenchALU* TBm = new TestbenchALU("TBm");
 
- sc_clock Clock("Clock", 10, SC_NS, 0.5, 0, SC_NS, true);
+ sc_clock Clock("Clock",2000,SC_MS);
 
- sc_signal<sc_uint<3> > op_sel;
+ sc_signal<sc_bv<4> > op_sel;
  sc_signal<bool> reset;
- sc_signal<sc_uint<32> > data1;
- sc_signal<sc_uint<32> > data2;
+ sc_signal<sc_uint<32> > d1;
+ sc_signal<sc_uint<32> > d2;
  sc_signal<sc_uint<32> > acc;
- sc_signal<sc_uint<8> > stat;
+ sc_signal<sc_bv<8> > stat;
 
  ALUm-> clk(Clock);
  ALUm-> op_sel(op_sel);
  ALUm-> reset(reset);
- ALUm-> data1(data1);
- ALUm-> data2(data2);
+ ALUm-> d1(d1);
+ ALUm-> d2(d2);
  ALUm-> acc(acc);
  ALUm-> stat(stat);
 
  TBm-> clk(Clock);
  TBm-> reset(reset);
  TBm-> op_sel(op_sel);
- TBm-> data1(data1);
- TBm-> data2(data2);
+ TBm-> d1(d1);
+ TBm-> d2(d2);
 
- sc_trace_file* file = sc_create_vcd_trace_file("ALUFile");
+ sc_trace_file* file = sc_create_vcd_trace_file("ALU");
  cout<< "Start simulation" <<endl;
  cout<< sc_time_stamp() <<"Creating VCD file:\n";
 
  sc_trace(file,Clock,"clock");
  sc_trace(file,reset,"reset");
- sc_trace(file,op_sel,"count_en");
- sc_trace(file,data1,"ud_ctrl");
- sc_trace(file,data2,"count_out");
- sc_trace(file,acc,"ovf_intr");
- sc_trace(file,stat,"unf_intr");
+ sc_trace(file,op_sel,"op_sel");
+ sc_trace(file,d1,"data1");
+ sc_trace(file,d2,"data2");
+ sc_trace(file,acc,"acc");
+ sc_trace(file,stat,"stat");
 
-
-
- cout << sc_time_stamp() << "start simulation";
+ cout << sc_time_stamp() << "  start simulation";
  sc_start();
  return 0;
-
  }
